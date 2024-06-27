11673976c3c44b072e7257c39208aa4f90358cd6
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-06-26 21:29:34 -0600

    Sections completition

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule],
+  imports: [RouterOutlet, MatIconModule, MatTooltipModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
