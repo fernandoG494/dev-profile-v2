a75eef76440b67e165dca2d927fa4b55920b4dc5
Fernando García (Incuerd0) <42920098+fernandoG494@users.noreply.github.com>
2024-06-30 22:32:38 -0600

    Selector color implemented

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatTooltipModule],
+  imports: [RouterOutlet, MatIconModule, MatTooltipModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
+  constructor(private router: Router) {}
+
+  getCurrentRoute(): string {
+    return this.router.url;
+  }
+
   handleContactButton = () => {
     console.log('contact');
   };
